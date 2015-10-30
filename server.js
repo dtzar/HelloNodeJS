7ec676c311f2f81360b3fcf04031a6bec8da1785
David Tesar <dtzar@hotmail.com>
2015-10-30 11:58:46 -0700

    Updated homepage

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.port || 1337
 
 app.get('/', function (req, res) {
-    res.send('Hello from GitHub to Azure Web App!');
+    res.send('Hello from GitHub!');
 });
 
 var server = app.listen(port, function () {
